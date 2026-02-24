100448c053af9f0123f72da2be7e1b44a3286939
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2026-02-24 11:25:33 +0700

    feat: fixing bug in view all tasks

--- src/features/viewTask.js
+++ src/features/viewTask.js
@@ -3,6 +3,7 @@ import fs from "fs";
 import { renderTable } from "../utils/table.js";
 import { renderFeatures } from "../home.js";
 import { homePage, rl } from "../../app.js";
+import { getDataFromFile } from "../services/getDataFromFile.js";
 
 const VIEW_ALL_TASK_FEATURES = [
   { id: 1, feature: "Edit one task status" },
@@ -14,34 +15,15 @@ const VIEW_ALL_TASK_FEATURES = [
 function handleAnswerViewAllTaskPage(answer) {}
 
 export async function viewAllTask() {
-  const readFile = fs.readFile("taskData.json", (error, data) => {
-    if (error.code === "ENOENT") {
-      console.log("No Task already, add new one! \n");
+  const task = await getDataFromFile(({ data, error }) => {
+    if (error?.code === "ENOENT") {
       homePage();
 
       return;
     }
 
-    return { error, data };
+    return data;
   });
 
-  // const { error, data } = readFile;
-
-  // if (error) {
-  //   console.log(error);
-  //   return;
-  // }
-
-  // const taskToRender = JSON.parse(data);
-  // renderTable(taskToRender);
-
-  // // Render features
-  // console.log(""); // Spacing
-  // renderFeatures(VIEW_ALL_TASK_FEATURES);
-
-  // // Render Question
-  // const answer = await rl.question("What do you want to do? ");
-
-  // // Function
-  // handleAnswerViewAllTaskPage(answer);
+  if (!task) return;
 }
